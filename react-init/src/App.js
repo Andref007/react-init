a39efb4306053ecd5da2e36c964f40511810f9e9
Andref007 <andrefps007@hotmail.com>
2022-08-25 20:57:40 -0300

    revisao-react

---
+++ react-init/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Livros</h1>
+
+      <div>
+        {/* <ul>
+          <li>Memórias de uma Gueixa</li>
+          <li>O Príncipe</li>
+          <li>Simarilion</li>
+        </ul> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
